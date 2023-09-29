d29744671bb09b6439f110e5ee7a5757440f6426
Jacob Louis <louisjacob8@gmail.com>
2023-09-29 12:43:40 +0530

    feat: added bootstrap, create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="container-fluid border-bottom border-warning">
+      <div className="row ">
+        <div
+          className="col-12 gap-5  d-flex justify-content-center justify-content-md-end align-middle mb-2 pt-5"
+          style={{ paddingRight: "10%" }}
+        >
+          <a href="#" className="text-white text-decoration-none ">
+            Home
+          </a>
+          <a href="#" className="text-white text-decoration-none">
+            About
+          </a>
+          <a href="#" className="text-white text-decoration-none">
+            Contact
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
